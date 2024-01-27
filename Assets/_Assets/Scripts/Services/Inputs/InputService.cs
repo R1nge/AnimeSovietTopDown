25795fecf0247ef7b9b901cd1b4f336bd38e2b74
R1nge <r1ngegamedev@gmail.com>
2024-01-27 19:39:09 +0300

    Made InputVector a property with private setter

--- Assets/_Assets/Scripts/Services/Inputs/InputService.cs
+++ Assets/_Assets/Scripts/Services/Inputs/InputService.cs
@@ -5,11 +5,11 @@ namespace _Assets.Scripts.Services.Inputs
 {
     public class InputService : ITickable  
     {
-        private Vector3 _input;
-        public Vector3 InputVector => _input;
+        public Vector3 InputVector { get; private set; }
+
         public void Tick()
         {
-            _input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+            InputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
         }
     }
 }
