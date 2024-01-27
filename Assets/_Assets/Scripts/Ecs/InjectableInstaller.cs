87275cf24b138eeeceb5178261c2adfc23d8c39e
R1nge <r1ngegamedev@gmail.com>
2024-01-27 19:04:54 +0300

    Morpeh ECS

---
+++ Assets/_Assets/Scripts/Ecs/InjectableInstaller.cs
@@ -0,0 +1,33 @@
+using Scellecs.Morpeh;
+using VContainer;
+
+namespace _Assets.Scripts.Ecs
+{
+    public class InjectableInstaller : Installer
+    {
+        [Inject] private IObjectResolver _objectResolver;
+        
+        private void Start()
+        {
+            foreach (var initializer in initializers)
+            {
+                _objectResolver.Inject(initializer);
+            }
+
+            foreach (var updateSystem in updateSystems)
+            {
+                _objectResolver.Inject(updateSystem.System);
+            }
+
+            foreach (var lateUpdateSystem in lateUpdateSystems)
+            {
+                _objectResolver.Inject(lateUpdateSystem.System);
+            }
+
+            foreach (var cleanupSystem in cleanupSystems)
+            {
+                _objectResolver.Inject(cleanupSystem.System);
+            }
+        }
+    }
+}
