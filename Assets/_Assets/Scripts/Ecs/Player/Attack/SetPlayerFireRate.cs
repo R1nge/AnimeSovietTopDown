08c74b03aea138f6ad20ff09bd906a28905293c5
R1nge <r1ngegamedev@gmail.com>
2024-01-29 11:54:51 +0300

    Set player fire rate from player stats

---
+++ Assets/_Assets/Scripts/Ecs/Player/Attack/SetPlayerFireRate.cs
@@ -0,0 +1,26 @@
+﻿using _Assets.Scripts.Services;
+using UnityEngine;
+using VContainer;
+
+namespace _Assets.Scripts.Ecs.Player.Attack
+{
+    using Scellecs.Morpeh;
+
+    [CreateAssetMenu(menuName = "ECS/Systems/" + nameof(SetPlayerFireRate))]
+    public sealed class SetPlayerFireRate : Scellecs.Morpeh.Systems.UpdateSystem
+    {
+        [Inject] private PlayerStatsService _playerStatsService;
+        private Filter _filter;
+
+        public override void OnAwake()
+        {
+            _filter = World.Filter.With<PlayerAttackComponent>().Build();
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            var player = _filter.First();
+            player.GetComponent<PlayerAttackComponent>().cooldown = 1f / _playerStatsService.PlayerStats.FireRate;
+        }
+    }
+}
